1fb362c804b2c173b594c630aa4aba0c933f5bfe
Daniel <maydanachi@gmail.com>
2022-06-21 22:17:03 -0400

    Implement login form styles

--- src/components/LoginForm.template.js
+++ src/components/LoginForm.template.js
@@ -1,7 +1,17 @@
 
 const template = document.createElement('template')
 template.innerHTML = /* html */ `
-    <div>BRMC</div>
+    <div class="container">
+        <div class="controls">
+            <div class="login active">LOG IN</div>
+            <div class="sign-up">SIGN UP</div>    
+        </div>
+        <div class="inputs">
+            <input class="mail" type="email" placeholder="E-mail"></input>
+            <input class="psw" type="text" placeholder="Password"></input>
+        </div>
+        <div class="using-google">USING GOOGLE</div>
+    </div>
 `
 
 export default template
